d05dd0e3f33782b39724b9d77703d609e30796f5
ziadfarahat2 <ziadfarahat2@gmail.com>
2026-06-18 00:42:48 +0200

    added routes

--- backend/src/server.js
+++ backend/src/server.js
@@ -4,9 +4,14 @@ import dotenv from "dotenv";
 import mongoose from "mongoose";
 
 import authRoutes from "./routes/auth.routes.js";
+import eventRoutes from "./routes/event.routes.js";
 import venueRoutes from "./routes/venue.routes.js";
+import bookingRoutes from "./routes/booking.routes.js";
+import taskRoutes from "./routes/task.routes.js";
+import guestRoutes from "./routes/guest.routes.js";
 import vendorRoutes from "./routes/vendor.routes.js";
-
+import invoiceRoutes from "./routes/invoice.routes.js";
+import feedbackRoutes from "./routes/feedback.routes.js";
 dotenv.config();
 
 const app = express();
@@ -21,8 +26,14 @@ app.get("/", (req, res) => {
 
 // Routes
 app.use("/api/auth", authRoutes);
+app.use("/api/events", eventRoutes);
 app.use("/api/venues", venueRoutes);
+app.use("/api/bookings", bookingRoutes);
+app.use("/api/tasks", taskRoutes);
+app.use("/api/guests", guestRoutes);
 app.use("/api/vendors", vendorRoutes);
+app.use("/api/invoices", invoiceRoutes);
+app.use("/api/feedback", feedbackRoutes);
 
 // Connect to MongoDB and start server
 mongoose
