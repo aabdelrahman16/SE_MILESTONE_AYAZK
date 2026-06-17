3b2320bc8116d04de5253926bba38744045fa6ba
ziadfarahat2 <ziadfarahat2@gmail.com>
2026-06-18 00:31:20 +0200

    Rename model from User to Event

--- backend/src/models/Event.js
+++ backend/src/models/Event.js
@@ -13,4 +13,4 @@ const userSchema = new mongoose.Schema({
   isActive: { type: Boolean, default: true }
 }, { timestamps: true });
 
-module.exports = mongoose.model("User", userSchema);
+module.exports = mongoose.model("Event", userSchema);
